c9cb9518cf197d5a898315056e39ca186729d998
Angie the Fairytale <konzeptmeister@gmail.com>
2014-11-08 20:20:43 +0100

    optimized

--- adjacency/data_convertor.js
+++ adjacency/data_convertor.js
@@ -1,63 +1,47 @@
+//converts groups to pairwise connections
 var convertGroupsToLinks = function(arrayOfGroups){
-    //converts groups to pairwise connections
 
-    var input = arrayOfGroups;
+    //output array
     var result = [];
     
-    var doublePush = function(rule, array){
-        array.push({
-            "if": rule.alarms[0],
-            "then": rule.alarms[1],
-            "hits": rule.supp,
-            "confidence": rule.conf
-        });
-        array.push({
-            "if": rule.alarms[1],
-            "then": rule.alarms[0],
-            "hits": rule.supp,
-            "confidence": rule.conf
-        });    
-        return array;
-    };
-
-    input.filter(function(rule){return rule.alarms.length == 2})
-        .forEach(function(rule){
-
-            // check if such element already exists in array
-            if(result.filter(function(d){
-                return d.if == rule.alarms[0] && d.then == rule.alarms[1]
-                    || d.if == rule.alarms[1] && d.then == rule.alarms[0];
-            }).length) log("duplicate found!")
-
-            // push the new pair
-            result = doublePush(rule, result);
-            rule.processed = true;
-        });
-
-    input = input.filter(function(rule){return !rule.processed});
-
+    //sort all the groups so that we will preocess shorter groups first
+    var input = arrayOfGroups.sort(function(a,b){ return b.alarms.length - a.alarms.length; })
+    
+    //for each rule get all pairwise combinations in alarms array
     input.forEach(function(rule){
         rule.alarms.forEach(function(a0){
             rule.alarms.forEach(function(a1){
+                
+                //skip self-pointing connections
                 if(a0==a1)return;
 
-                // check if such element already exists in array
+                //check if such link already exists in results array. do nothing if yes
                 if(result.filter(function(d){
-                    return d.if == a0 && d.then == a1
-                        || d.if == a1 && d.then == a0;
+                    return d.if == a0 && d.then == a1 || d.if == a1 && d.then == a0;
                 }).length)return;
 
-                // push the new pair
-                result = doublePush({alarms: [a0,a1], supp: rule.supp, conf: rule.conf}, result);      
-
-            })
-        })
-    })
+                // push the new pair twice (thus, links are not directional)
+                result.push({
+                    "if": a0,
+                    "then": a1,
+                    "hits": rule.supp,
+                    "confidence": rule.conf
+                });
+                result.push({
+                    "if": a1,
+                    "then": a0,
+                    "hits": rule.supp,
+                    "confidence": rule.conf
+                });    
+            
+            }); // alarm a1
+        }); // alarm a0
+    }); //rule
 
 
     //simple validation
-    var badRules = result.filter(function(rule){return rule.if && rule.then && rule.support && rule.confidence});
-    if (badRules.length) console.log("not all elements in the result have properties", badRules);
+    var badRules = result.filter(function(rule){return rule.if && rule.then && rule.hits && rule.confidence});
+    if (badRules.length != result.length) console.warn("not all elements in the result have properties", badRules);
 
     return result;
 }
